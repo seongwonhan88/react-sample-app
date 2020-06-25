6b16933fa7ce4ff12c0f02978b641cf452285c7b
Seongwon Han <seongwonhan88@gmail.com>
2020-06-25 18:08:52 -0400

    navbar setup

---
+++ src/components/Navigation/NavItems/NavItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './NavItems.module.css';
+import NavItem from './NavItem/NavItem';
+
+const navItems = (props) => (
+    <ul className={styles.NavItems}>
+        <NavItem link="/" active>BurgerBuilder</NavItem>
+        <NavItem link="/">Checkout</NavItem>
+    </ul>
+);
+
+export default navItems;
